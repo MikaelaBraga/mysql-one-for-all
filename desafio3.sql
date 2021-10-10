aa943e8cf7c8504e789fe269a8a59a6e97d89226
MIkaela Braga <bragamikaela@gmail.com>
2021-10-10 19:54:22 -0300

    Desafio 3 completo

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+CREATE VIEW historico_reproducao_usuarios AS
+    SELECT 
+        u.nome_usuario AS `usuario`, c.nome_cancao AS `nome`
+    FROM
+        SpotifyClone.Usuários AS u
+            INNER JOIN
+        SpotifyClone.Histórico_Reprodução AS hr ON hr.usuario_id = u.usuario_id
+            INNER JOIN
+        SpotifyClone.Canções AS c ON c.cancoes_id = hr.cancoes_id
+    ORDER BY u.nome_usuario, c.nome_cancao ASC;
