8979637da1ab04a02fa2bf4967c0d8752f6e8347
MIkaela Braga <bragamikaela@gmail.com>
2021-10-11 00:00:32 -0300

    Desafio 8 completo

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+DELIMITER $$
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON SpotifyClone.Usuários
+	FOR EACH ROW
+    BEGIN
+    DELETE FROM SpotifyClone.Seguidores_Artistas
+    WHERE OLD.usuario_id = SpotifyClone.Seguidores_Artistas.usuario_id;
+DELETE FROM SpotifyClone.Histórico_Reprodução 
+WHERE
+    OLD.usuario_id = SpotifyClone.Histórico_Reprodução.usuario_id;
+  END $$
+
+DELIMITER ;
