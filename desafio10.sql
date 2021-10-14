49e43fb4160c91177b622abd1c74e780f162ea29
MIkaela Braga <bragamikaela@gmail.com>
2021-10-14 00:25:21 -0300

    Desafio 10 completo

---
+++ desafio10.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(userId INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE quantMusic INT;
+SELECT COUNT(*)
+FROM SpotifyClone.Histórico_Reprodução AS h
+INNER JOIN SpotifyClone.Usuários AS us ON us.usuario_id = h.usuario_id
+WHERE us.usuario_id = userId INTO quantMusic;
+RETURN quantMusic;
+END $$
+
+DELIMITER ;
+ 
